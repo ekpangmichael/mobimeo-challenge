395332d3e24fb92e6c6b24c35aef10b002fe53d2
Michael Ekpang <michael.ekpang@flutterwavego.com>
2021-05-16 22:36:25 +0100

    setup app

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const welcomeNote = require('./helpers/welcomeNotes');
+const app = express();
+
+const port = process.env.PORT || 5678;
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(require('./routes'));
+
+app.use('/', (req, res) => {
+  const random = Math.floor(Math.random() * 10);
+  res.status(200).json({
+    message: 'Welcome!',
+    note: 'figure out the next quote 😁',
+    quote: welcomeNote[random],
+  });
+});
+
+app.listen(port, () => console.log(`\n Server started on port ${port}`));
