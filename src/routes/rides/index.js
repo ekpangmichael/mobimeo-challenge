e284e046ac8904ff2b7b336eaa8d9e1e3ae33157
Michael Ekpang <michael.ekpang@flutterwavego.com>
2021-05-16 23:02:30 +0100

    added rides route

---
+++ src/routes/rides/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+fs.readdirSync(__dirname).forEach(route => {
+  
+  const [routeVal] = route.split('.');
+  if (routeVal === 'index' || routeVal === 'docs') return ;
+  router.use(require(`./${routeVal}.js`));
+ 
+});
+
+module.exports = router;
+
